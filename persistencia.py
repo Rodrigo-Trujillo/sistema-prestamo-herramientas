33491736aac8a3b267b7e61ee979332269c1d92e
Rodrigo Trujillo <trujillo.rodrigo201@gmail.com>
2026-08-08 20:19:23 -0500

    persistencia.py: agregada ruta completa con CARPETA_DATOS

--- persistencia.py
+++ persistencia.py
@@ -5,13 +5,22 @@ import os
 CARPETA_DATOS = "datos"
 
 def cargar(nombre_archivo):
-    if  not os.path.exists(nombre_archivo):  
-        return []                            
-    with open(nombre_archivo, "r", encoding='utf-8') as archivo:
-        return json.load(archivo)
+    ruta = os.path.join(CARPETA_DATOS, nombre_archivo)
+    if not os.path.exists(ruta):
+        return []
+    try:
+        with open(ruta, "r", encoding='utf-8') as archivo:
+            return json.load(archivo)
+    except json.JSONDecodeError:
+        print("El archivo está dañado. Se inicia con lista vacía.")
+        return []
+    except Exception as error:
+        print("Error al leer el archivo:", error)
+        return []
 
 def guardar(nombre_archivo, datos):
-    with open(nombre_archivo, "w", encoding='utf-8') as archivo:
+    ruta = os.path.join(CARPETA_DATOS, nombre_archivo)
+    with open(ruta, "w", encoding='utf-8') as archivo:
         json.dump(datos, archivo)
 
 def siguiente_id(lista):

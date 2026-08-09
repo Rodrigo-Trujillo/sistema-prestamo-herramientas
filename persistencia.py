b934cae5e73d2f6978ccc022a5c01b3dbfdb7aeb
Rodrigo Trujillo <trujillo.rodrigo201@gmail.com>
2026-08-08 19:30:46 -0500

    persistencia.py: función cargar lista

---
+++ persistencia.py
@@ -0,0 +1,13 @@
+import json 
+import os 
+
+
+CARPETA_DATOS = "datos"
+
+def cargar(nombre_archivo):
+    if  not os.path.exists(nombre_archivo):  
+        return []                            
+    with open(nombre_archivo, "r", encoding='utf-8') as archivo:
+        return json.load(archivo)
+
+
